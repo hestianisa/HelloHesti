3bf84b01e845b8458232f3cd9129e12099196d82
hesti <hestianisa94@gmail.com>
2019-09-05 14:17:09 +0700

    menambah lifecycle

--- app/src/main/java/com/example/hellohesti/MainActivity.java
+++ app/src/main/java/com/example/hellohesti/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hellohesti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "App on Stop", Toast.LENGTH_SHORT).show();
     }
 }
